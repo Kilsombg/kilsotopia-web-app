e1e1005883bf010afabed44bb607f87a478b4ec4
Kilsom <ivankamkam@abv.bg>
2023-07-01 10:41:16 +0300

    calendar visual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calendar-web-app';
+  month : string[] = [];
+
+  constructor() {
+    for(let i=1;i<31;i++)
+      this.month.push(String(i));
+  }
 }
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent {
+@Input() month : string[];
+}
