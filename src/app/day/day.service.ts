d016a46da6e6d4c55d585f796d8737d5b3f2aaee
Kilsom <47924593+Kilsombg@users.noreply.github.com>
2023-07-11 21:53:28 +0300

    Update day.service.ts
    work in progress

--- src/app/day/day.service.ts
+++ src/app/day/day.service.ts
@@ -4,12 +4,13 @@ import { Day } from "./day.model";
 
 @Injectable()
 export class DayService {
-    tempDate : Day = new Day(new Date(2021,0,5));
+    tempDate : Day = this.createDay(new Date(2021,0,5));
     selectedDay : Subject<Day> = new BehaviorSubject<Day>(this.tempDate);
 
     setSelectedDay(newDay : Day) {
         newDay.isSelected = true;
         console.log("previous: ",this.selectedDay.asObservable());
+        this.selectedDay.subscribe()
         this.selectedDay.next(newDay);
         console.log("current: ",this.selectedDay.asObservable());
     }
