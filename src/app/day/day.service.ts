3e831a652caac4a4e56d4a5c9542a6c38f7aa2a8
Kilsom <47924593+Kilsombg@users.noreply.github.com>
2024-06-13 13:31:30 +0300

    work in progress

--- src/app/day/day.service.ts
+++ src/app/day/day.service.ts
@@ -1,21 +1,14 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Subject } from "rxjs";
+import { BehaviorSubject, Subject, map } from "rxjs";
 import { Day } from "./day.model";
 
 @Injectable()
 export class DayService {
-    tempDate : Day = this.createDay(new Date(2021,0,5));
-    selectedDay : Subject<Day> = new BehaviorSubject<Day>(this.tempDate);
-
-    setSelectedDay(newDay : Day) {
-        newDay.isSelected = true;
-        console.log("previous: ",this.selectedDay.asObservable());
-        this.selectedDay.subscribe()
-        this.selectedDay.next(newDay);
-        console.log("current: ",this.selectedDay.asObservable());
+    setSelectedDay(newDay: Day): void {
+        newDay.isSelected = !newDay.isSelected;
     }
 
-    createDay(date: Date) : Day {
+    createDay(date: Date): Day {
         return new Day(date);
     }
 }
