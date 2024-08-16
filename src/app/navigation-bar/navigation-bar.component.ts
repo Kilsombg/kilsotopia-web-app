f7906a318fd98791702326d6bffe7c51aa920f77
Kilsom <47924593+Kilsombg@users.noreply.github.com>
2024-08-16 16:03:33 +0300

    added section - about kilsotopia

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RoutingService } from '../services/routing.service';
-import { ActivatedRoute } from '@angular/router';
 
 enum Items {
   Home,
