34cd671c8e48df2612e7dff666a4888d5bb28313
Kilsom <47924593+Kilsombg@users.noreply.github.com>
2024-08-18 22:34:20 +0300

    icon to scroll to calendar section

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { RoutingService } from '../services/routing.service';
 import { NavigationService } from '../services/navigation.service';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
@@ -12,9 +13,14 @@ export class HomepageComponent {
   constructor(
     private router: Router,
     private routingService: RoutingService,
-  private navigationService: NavigationService) { }
+    private navigationService: NavigationService,
+    private scroller: ViewportScroller) { }
 
-  onCalendarBtnClick() {
+  onCalendarIconClick(): void {
+    this.scroller.scrollToAnchor("section-calendar");
+  }
+
+  onCalendarBtnClick(): void {
     var currentUrl = this.routingService.getCurrentUrl();
 
     this.navigationService.alterActiveNavItem(currentUrl);
